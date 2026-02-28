633e25584784de2425452df46e05bbd47da0283a
filipswiszcz <65617364+filipswiszcz@users.noreply.github.com>
2026-02-28 16:55:41 +0100

    game window

--- src/game.c
+++ src/game.c
@@ -1,6 +1,91 @@
+#include <stdint.h>
 #include <stdio.h>
+#include <stdlib.h>
+
+// #include <libmath/math.h>
+
+#ifdef __APPLE__
+    #define GL_SILENCE_DEPRECATION
+    #define GLFW_INCLUDE_GLCOREARB
+#else
+    #include <GL/glew.h>
+    #include <GLFW/glfw3.h>
+#endif
+
+#define WINDOW_WIDTH 800
+#define WINDOW_HEIGHT 600
+#define WINDOW_NAME "BattleArena 2D (Build v0.0.1)"
+
+#define ASSERT(_e, ...) if (!(_e)) {fprintf(stderr, __VA_ARGS__); exit(1);}
+
+// GAME
+
+static struct {
+    GLFWwindow *window;
+} context;
+
+// TODO
+// game icon
+// memory arena
+// assets
+// ..
+
+void game_init(void) {
+    // GLFW
+    ASSERT(glfwInit(), "Failed to initialize OpenGL!");
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+#ifdef __APPLE__
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
+
+    context.window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_NAME, NULL, NULL);
+    ASSERT(context.window, "Failed to create GLFW window!");
+
+    glfwMakeContextCurrent(context.window);
+    // glfwSetInputMode(context.window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+
+    // GLEW
+#ifndef __APPLE__
+    glewExperimental = 1;
+    int32_t glew_err = glewInit();
+    ASSERT(glew_err == 0 || glew_err == 4, "Failed to initialize GLEW!");
+#endif
+
+    // OPENGL
+    // glEnable(GL_DEPTH_TEST);
+    // glEnable(GL_PROGRAM_POINT_SIZE);
+    // glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+}
+
+void game_update(void) {
+    while (!glfwWindowShouldClose(context.window)) {
+
+        // OPENGL
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        //..
+
+        // OPENGL
+        glfwSwapBuffers(context.window);
+        glfwPollEvents();
+
+    }
+}
+
+void game_stop(void) {
+    glfwDestroyWindow(context.window);
+    glfwTerminate();
+}
+
+// MAIN
 
 int main(void) {
-    printf("xx\n");
+    game_init();
+    game_update();
+    game_stop();
     return 0;
 }
