708b3f84ac353d9476a6e35a4f088b068af2e563
filipswiszcz <65617364+filipswiszcz@users.noreply.github.com>
2026-03-12 01:47:53 +0100

    glew32.dll and glfw3.dll linking

--- build.bat
+++ build.bat
@@ -1,7 +1,12 @@
 @echo off
 if not exist build mkdir build
 
-cl.exe /nologo /std:c11 /W4 /I.\lib src\game.c /Febuild\game.exe /Fobuild\ ^
-    glfw3dll.lib glew32.lib opengl32.lib gdi32.lib user32.lib shell32.lib
+set vcpkg=D:\vcpkg\installed\x64-windows
+
+cl.exe /nologo /std:c11 /W4 /I"%vcpkg%\include" /I./lib src\game.c /Febuild\game.exe /Fobuild\ ^
+    /link /LIBPATH:"%vcpkg%\lib" glfw3dll.lib glew32.lib opengl32.lib gdi32.lib user32.lib shell32.lib
+
+copy /y "%vcpkg%\bin\glew32.dll" build\ >nul
+copy /y "%vcpkg%\bin\glfw3.dll" build\ >nul
 
 echo Build complete!
