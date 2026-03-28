4effc655d36a35f252ce6398bc8937ca326f8718
filipswiszcz <65617364+filipswiszcz@users.noreply.github.com>
2026-03-28 13:43:39 +0100

    exit with compilation error, when build fails

--- build.bat
+++ build.bat
@@ -6,6 +6,11 @@ set vcpkg=D:\vcpkg\installed\x64-windows
 cl.exe /nologo /std:c11 /W4 /DDEBUG /I"%vcpkg%\include" /I./lib src\game.c /Febuild\game.exe /Fobuild\ ^
     /link /LIBPATH:"%vcpkg%\lib" glfw3dll.lib glew32.lib opengl32.lib gdi32.lib user32.lib shell32.lib
 
+if %errorlevel% neq 0 (
+    echo Error!
+    exit /b %errorlevel%
+)
+
 copy /y "%vcpkg%\bin\glew32.dll" build\ >nul
 copy /y "%vcpkg%\bin\glfw3.dll" build\ >nul
 
