bf15185388c7d680ff1c04c7fa1f29555b3e8c16
filipswiszcz <65617364+filipswiszcz@users.noreply.github.com>
2026-04-16 11:01:46 +0200

    add glitch effect

--- shader/sprite.fs
+++ shader/sprite.fs
@@ -1,12 +1,39 @@
 #version 410 core
 
 in vec2 t_Uv;
+in vec4 t_Bound;
 
 out vec4 o_Color;
 
 uniform sampler2D u_Texture;
 uniform vec3 u_Color;
 
-void main() {
-    o_Color = vec4(u_Color, 1.0f) * texture(u_Texture, t_Uv);
+uniform float u_GlitchTime;
+uniform float u_GlitchIntensity;
+
+float random(vec2 seed) {
+    return fract(sin(dot(seed, vec2(12.9898, 78.233))) * 43758.5453123);
+}
+
+void main() {    
+    vec2 c_Uv = t_Uv;
+
+    float c_TimeStep = floor(u_GlitchTime * 15.0f);
+    float c_Slice = floor(c_Uv.y * 30.0f);
+    float c_Noise = random(vec2(c_TimeStep, c_Slice));
+
+    if (c_Noise < u_GlitchIntensity * 0.4f) {
+        c_Uv.x += (random(vec2(c_TimeStep, c_Slice)) - 0.5f) * 0.1f * u_GlitchIntensity;
+    }
+
+    float c_Split = 0.015 * u_GlitchIntensity;
+
+    vec2 c_UvR = clamp(c_Uv + vec2(c_Split, 0.0f), (t_Bound.xy), (t_Bound.zw));
+    vec2 c_UvB = clamp(c_Uv - vec2(c_Split, 0.0f), (t_Bound.xy), (t_Bound.zw));
+
+    float R = texture(u_Texture, c_UvR).r;
+    vec4 S = texture(u_Texture, c_Uv);
+    float B = texture(u_Texture, c_UvB).b;
+
+    o_Color = vec4(vec3(R, S.g, B) * u_Color, S.a);
 }
--- shader/sprite.vs
+++ shader/sprite.vs
@@ -4,6 +4,7 @@ layout (location = 0) in vec2 l_Position;
 layout (location = 1) in vec2 l_Uv;
 
 out vec2 t_Uv;
+out vec4 t_Bound;
 
 uniform mat4 u_Projection;
 uniform mat4 u_Model;
@@ -14,7 +15,12 @@ uniform int u_Mirror;
 
 void main() {
     gl_Position = u_Projection * u_Model * vec4(l_Position, 0.0f, 1.0f);
-    vec2 c_Uv = (l_Uv * u_Scale) + u_Offset;
-    if (u_Mirror == 1) c_Uv.x = 1.0f - c_Uv.x;
-    t_Uv = c_Uv;
+
+    vec2 c_Uv = l_Uv;
+    if (u_Mirror == 1) {
+        c_Uv.x = 1.0f - c_Uv.x;
+    }
+
+    t_Uv = (c_Uv * u_Scale) + u_Offset;
+    t_Bound = vec4(u_Offset, u_Offset + u_Scale);
 }
