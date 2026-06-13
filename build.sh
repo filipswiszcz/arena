6e37c1d2f76441807d093c71f88e758f200ef74b
filipswiszcz <65617364+filipswiszcz@users.noreply.github.com>
2026-06-13 22:53:00 +0200

    refactor build scripts

--- build.sh
+++ build.sh
@@ -2,15 +2,64 @@
 
 set -e
 
-mkdir -p build
-
-if [ "$(uname)" == "Darwin" ]; then
-    clang src/game.c -o build/game -std=c99 -Wall -Wextra \
-        -I./lib -lglfw -framework Cocoa -framework OpenGL -framework IOKit
+if [ "$(uname)" == "Linux" ]; then
+    LIBS=(-lglfw -lGLEW -lGL -lX11 -lpthread -ldl -lm)
+    CC="gcc"
+elif [ "$(uname)" == "Darwin" ]; then
+    LIBS=(-lglfw -framework OpenGL -framework Cocoa -framework IOKit -framework CoreVideo)
+    CC="clang"
 else
-    gcc src/game.c -o build/game -std=c99 -Wall -Wextra \
-        -I./lib -I/usr/include/GLFW -I/usr/include/GL \
-        -lglfw -lGLEW -lGL -lm
+    echo "UNSUPPORTED OS"
+    exit 1
 fi
 
-echo "Build complete!"
+mkdir -p bin
+
+MODE="$1"
+
+case "$MODE" in
+    debug)
+        FLAGS=(-g -O0 -DDEBUG -Wall -Wextra -std=c99)
+        ;;
+    dev)
+        FLAGS=(-g -O0 -Wall -Wextra -std=c99)
+        ;;
+    release)
+        FLAGS=(-O3 -Wall -Wextra -std=c99)
+        ;;
+    *)
+        echo "BUILD AVAILABLE OPTIONS: debug, dev, release"
+        exit 1
+        ;;
+esac
+
+rm -rf build
+mkdir -p build
+
+$CC "${FLAGS[@]}" \
+    -I./lib \
+    src/game.c \
+    "${LIBS[@]}" \
+    -o build/game
+
+rm -f bin/game
+rm -rf bin/res
+
+if [ "$MODE" = "release" ]; then
+    cp -f build/game bin/
+    if [ -d "res" ]; then
+        if command -v rsync >/dev/null 2>&1; then
+            rsync -av --delete res/ bin/res/ >/dev/null
+        else
+            cp -R res bin/res
+        fi
+    fi
+else
+    # Fixed typo from 'egine' to 'game'
+    ln -sf ../build/game bin/game
+    if [ -d "res" ]; then
+        ln -s ../res bin/res
+    fi
+fi
+
+echo "BUILD COMPLETE"
